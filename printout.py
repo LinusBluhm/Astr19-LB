0ef1cfef39fa505a4c911e44e407a314d0556b86
LinusBluhm <156133653+LinusBluhm@users.noreply.github.com>
2024-01-10 13:25:45 -0800

    Add files via upload

---
+++ printout.py
@@ -0,0 +1,2 @@
+print('Linus Bluhm\nMasculine Pronouns\n')
+print("Superlatives such as favorite are difficult to use without qualification, \nbut at the moment of writing I'd say my favorite movie is The Lighthouse, \nand my favorite food is baklava.")
