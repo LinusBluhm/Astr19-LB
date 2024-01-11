0206b01444c51d179c2e0b001d076f2587941354
LinusBluhm <156133653+LinusBluhm@users.noreply.github.com>
2024-01-11 11:44:23 -0800

    Add files via upload

---
+++ Numbers.py
@@ -0,0 +1,17 @@
+Float1 = 5.0
+Float2 = 4.0
+Int1 = 3
+Int2 = 7
+
+Sumfloat = Float1+Float2
+DiffInt = abs(Int1-Int2)
+Product = Int1*Float1
+
+print(Sumfloat)
+print(type(Sumfloat))
+
+print(DiffInt)
+print(type(DiffInt))
+
+print(Product)
+print(type(Product))
